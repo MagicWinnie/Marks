3de7e41b45e3a45248e6870c740d994f0b00127a
Dmitrii <39788810+MagicWinnie@users.noreply.github.com>
2020-01-23 07:18:46 +0700

    Update dnevnik.py

--- dnevnik.py
+++ dnevnik.py
@@ -1,9 +1,10 @@
+#TODO: Fix problems with an url to the latest year
+
 # -*- coding: utf-8 -*-
 import requests
 from lxml import etree
 from bs4 import BeautifulSoup
 from lxml import html
-from numpy import savetxt
 import numpy as np
 import pandas as pd
 
@@ -41,7 +42,6 @@ session = requests.session()
 
 r = session.post(LOGINURL, data=formdata, headers=req_headers, allow_redirects=False)
 
-
 html_marks = session.get(DATAURL,  headers=req_headers2, allow_redirects=True).text
 soup = BeautifulSoup(html_marks,features="lxml")
 mark_list = soup.find_all('div', class_='cc')
@@ -69,12 +69,10 @@ soup3 = BeautifulSoup(html_third,features="lxml")
 mark_lists = soup3.find_all('table')
 html_string = str(mark_lists)
 df = pd.read_html(html_string)[0]
-#df = dfs[0]
-#print(df.columns)
 df = df.drop("Комментарий учителя",axis=1)
 df = df.drop("Присутствие",axis=1)
 df = df.dropna()
-#print(df)
+
 dates = df[['Дата и время']].to_numpy().ravel()
 marks_temp = []
 mark_lists = BeautifulSoup(str(mark_lists),features="lxml")
@@ -88,13 +86,11 @@ for link in marks_temp:
 
 old_date = df['Дата и время'].to_numpy()
 old_marks = df['Оценки'].to_numpy()
-#old_titles = df[]
-#print(df)
-#print(old_marks)
+
+#fixing errors with multiple marks per lesson
 new_titles = []
 new_date = []
 new_marks = []
-#print(len(old_marks), len(titles))
 for i in range(len(old_marks)):
     if len(str(old_marks[i]).split())>1:
         new_marks += list(map(str, old_marks[i].split()))
@@ -106,8 +102,8 @@ for i in range(len(old_marks)):
         new_marks.append(old_marks[i])
         new_date.append(old_date[i].replace("\xa0", " "))
         new_titles.append(titles[i])
-#print(new_titles, new_marks, new_date, len(new_titles), len(new_marks), len(new_date))
+
 df_new = pd.DataFrame({"Date":new_date, "Mark":new_marks, "Title": new_titles})
-# print(df_new)
 df_new.to_csv('marks'+str(values[sub])+'.csv',index=False, encoding='utf-8-sig')
+
 print("Done")
